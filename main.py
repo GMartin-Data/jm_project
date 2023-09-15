f07d63c0a79c4ae6f7bdfb9b63b8cc57c773923d
Greg <gregory.martin.data@gmail.com>
2023-09-15 11:11:57 +0200

    Implemented get_adzuna_cats() with simple output printing

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+
+from dotenv import load_dotenv
+import httpx
+from rich import print
+
+
+def configure() -> None:
+    """Set working environment"""
+    load_dotenv()
+    global ADZUNA_URL, ADZUNA_ID, ADZUNA_KEY
+    ADZUNA_URL = os.getenv('adzuna_url')
+    ADZUNA_ID = os.getenv('adzuna_id')
+    ADZUNA_KEY = os.getenv('adzuna_key')
+    
+    
+def create_client() -> httpx.Client:
+    """Create and configure a httpx Client for requesting"""
+    c = httpx.Client()
+    c.headers.update({
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+                Chrome/116.0.0.0 Safari/537.36',
+    })
+    return c
+
+
+def get_adzuna_cats():
+    url = f'{ADZUNA_URL}/jobs/fr/categories'
+    cli = create_client()
+    resp = cli.get(
+        url,
+        params = {
+            'app_id': ADZUNA_ID,
+            'app_key': ADZUNA_KEY
+        }
+    )
+    print(resp.text)
+
+
+if __name__ == '__main__':
+    configure()
+    get_adzuna_cats() 
