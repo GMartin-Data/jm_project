1b01637fe4882de535af1f1b90ac5555bdb0d9da
Greg <gregory.martin.data@gmail.com>
2023-09-15 18:49:28 +0200

    1st implementation of get_muse_job_page

--- the_muse.py
+++ the_muse.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import List, Optional
 
 from dotenv import load_dotenv
 import httpx
@@ -8,13 +9,18 @@ from rich import print
 from utils import get_timestamp
 
 
+CHOSEN_CATS = ['Computer and IT', 'IT',
+               'Data and Analytics', 'Data Science',
+               'Design and UX', 'UX',
+               'Software Engineer', 'Software Engineering']
+
+
 def configure() -> None:
     """Set working environment"""
     load_dotenv()
-    global MUSE_PARAMS
-    MUSE_PARAMS = {
-        'api_key': os.getenv('muse_key')
-        }
+    global MUSE_KEY, MUSE_URL
+    MUSE_KEY = os.getenv('muse_key')
+    MUSE_URL = os.getenv('muse_url')
     
 
 def create_client() -> httpx.Client:
@@ -24,3 +30,43 @@ def create_client() -> httpx.Client:
         'Content-Type': 'application/json',
         'User-Agent': os.getenv('user_agent')
         })
+    return c
+
+
+def get_muse_jobs_page(client: httpx.Client,
+                       page: int,
+                       category: List[str],
+                       company: Optional[List[str]] = None,
+                       level: Optional[List[str]] = None,
+                       location: Optional[List[str]] = None):
+    """Get the 20 job results of one page from The Muse API"""
+    base_params = {
+            'api_key': MUSE_KEY,
+            'page': page,
+            'category': category, 
+        }
+    # Didn't find anything smarter for optional parameters update
+    if company:
+        base_params.update({'company': company})
+    if level:
+        base_params.update({'level': level})
+    if location:
+        base_params.update({'location': location})
+    
+    
+    resp = client.get(
+        MUSE_URL,
+        params = base_params
+    )
+    resp.raise_for_status()
+    json_resp = resp.json()
+    
+    print(json_resp)
+    
+
+if __name__ == '__main__':
+    configure()
+    print(f'{MUSE_KEY = }')
+    print(f'{MUSE_URL = }')
+    cli = create_client()
+    get_muse_jobs_page(cli, 1, CHOSEN_CATS)
