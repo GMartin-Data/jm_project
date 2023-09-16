f4e0b2aa4e8d7029d55a1c42e88c898eaaac3ce3
Greg <gregory.martin.data@gmail.com>
2023-09-16 09:12:51 +0200

    Added a timer decorator

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 from datetime import datetime
+import functools
+import time
 
 
 def get_timestamp():
     """Help for file horodating"""
     return datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
+
+def timer(func):
+  """Print the runtime of the decorated function"""
+  @functools.wraps(func)
+  def wrapper_timer(*args, **kwargs):
+    # Before
+    start_time = time.perf_counter()
+    # Calling the function and storing value to return
+    value = func(*args, **kwargs)
+    # After
+    end_time = time.perf_counter()
+    run_time = end_time - start_time
+    print(f"Finished {func.__name__!r} in {run_time: .4f} seconds.")
+    return value
+  return wrapper_timer
